d6d14361dde4b9fc766705dbba36b91b985e7d81
ashimraja <rajaasim652gmail.com>
2024-12-24 14:56:14 +0530

    2nd lecture

---
+++ BTECHCEC/src/comp1.jsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+class MyComponent extends React.Component{
+    render(){
+        return (
+            <div>
+                Hello World
+                <h1>React h1</h1>
+                <h2>React h2</h2>
+                <h3>React h3</h3>
+                <input type="button"/>
+                <input type="checkbox"/>
+                <input type="color"/>
+                <input type="date"/>
+                <input type="datetime-local"/>
+                <input type="email"></input>
+            </div>
+        )
+    }
+}
+export default MyComponent
