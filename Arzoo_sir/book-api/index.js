dd5511189766b80bd01da209c4f4389526185638
ashimraja <rajaasim652@gmail.com>
2025-01-17 23:44:44 +0530

    lecture

--- Arzoo_sir/book-api/index.js
+++ Arzoo_sir/book-api/index.js
@@ -41,4 +41,12 @@ app.delete('/:id',(req,res)=>{
     );
     res.json(books);
 })
+//to fetch details based on id
+app.get('/:id',(req,res)=>{
+    const id= req.params.id;
+    selBook=books.filter((book)=> 
+        book.id!=id 
+    );
+    res.json(selBook);
+})
 app.listen(port,()=>console.log(`Example app listening on port ${port}`));
